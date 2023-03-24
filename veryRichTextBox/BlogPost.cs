455b732a67a716ae331d0f8b2e9e28add4cd2174
najimsultani <najimsultani2014@gmail.com>
2023-03-24 15:17:55 -0700

    Initial commit

---
+++ veryRichTextBox/BlogPost.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows;
+
+namespace veryRichTextBox
+{
+    public class BlogPost
+    {
+        string _subjectLine;
+        string _bodyText;
+        Color _subjectColor;
+        DateTime _timeStampe;
+
+
+        public BlogPost(string subjectLine, string bodyText, Color userColor)
+        {
+            _subjectLine = subjectLine;
+            _bodyText = bodyText;
+            _subjectColor= userColor;
+            _timeStampe = DateTime.Now; 
+        }
+
+        public string SubkectLine { get => _subjectLine; set => _subjectLine = value; }
+        public string BodyText { get => _bodyText; set => _bodyText = value; }
+
+        public Run HeaderFormatted(string subjectLine)
+        {
+            Run headerRun = new Run(subjectLine);
+            headerRun.Foreground = new SolidColorBrush(_subjectColor);//usercolor
+            headerRun.FontSize = 24;//font
+            headerRun.FontStyle = FontStyles.Oblique;
+
+            return headerRun;
+        }
+        //body formatted
+        public Run BodyFormatted(string bodyText)
+        {
+            Run runNewBody = new Run(bodyText);
+            runNewBody.FontSize = 16;//body font size
+
+            return runNewBody;
+        }
+
+        public Paragraph BlogPostFormatted()
+        {
+            //get the flow document
+            //create a paragraph
+            Paragraph newParagraph = new Paragraph();
+
+            //create a run
+            string subjectLine = _subjectLine;
+            string bodyText = _bodyText;
+
+            Run header = HeaderFormatted(subjectLine);
+            Run body = BodyFormatted(bodyText);
+
+            //add to paragrph
+            newParagraph.Inlines.Add(header);
+            newParagraph.Inlines.Add("\n");
+            newParagraph.Inlines.Add(body);
+
+            //SubjectLine
+            //new Line
+            //body Text
+
+            return newParagraph;
+
+        }
+
+
+
+        public override string ToString()
+        {
+            return _timeStampe.ToShortTimeString() + " " + _subjectLine;
+        }
+
+    }//BlogPost
+}//namespace
---
+++ veryRichTextBox/MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//Najim Sultani
+//Rich Text Box ( Advanced )
+//3-24-23
+
+namespace veryRichTextBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //FlowDocument fdDisplay = new FlowDocument();
+        //1st Item: FlowDocument
+        //2nd Item: Paragraph - Paragragh ( Block Element )
+        //3rd Item: Run - Run ( Inline Element )
+
+        FlowDocument fdDisplay = new FlowDocument();
+        List<BlogPost> blogPosts = new List<BlogPost>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            rtbDisplay.Document = fdDisplay;
+            
+
+
+        }
+
+        public void Itworked() //not in use
+        {
+            Paragraph para1 = new Paragraph();
+            Paragraph para2 = new Paragraph();
+
+            Run run1 = new Run("Programming 122 - Advanced Rich Text Box");
+            Run run2 = new Run("You are right");
+            //run1.Background = new SolidColorBrush(Colors.CadetBlue);
+            //creating a sentence 
+
+            //adding a sentence to our paragraph
+            //added our Run
+            //para1.Background = new SolidColorBrush(Colors.Orange);
+            para1.Inlines.Add(run1);
+            para1.Inlines.Add("\n");
+            para1.Inlines.Add(run2);
+
+
+            para1.Inlines.Add(new Run("Does this Display"));
+
+            para2.Inlines.Add("\n");
+            para2.Inlines.Add(run1);
+
+            //Adding the paragraph to our document
+            fdDisplay.Blocks.Add(para1);
+            fdDisplay.Blocks.Add(para2);
+
+            //fdDisplay.Background = new SolidColorBrush(Colors.Purple);  
+
+            //Adding our document to our Rich Text Box
+            //Finding color with Rgb
+            //rtbDisplay.Background = new SolidColorBrush(Color.FromRgb(255,133,133));    
+            rtbDisplay.Document = fdDisplay;
+
+            //R - RED
+            //G - GREEN
+            //B - BLUE
+
+        }
+        public void Example() //not in use
+        {
+            Paragraph p = new Paragraph();
+            Run r = new Run("Hi all");
+            r.FontWeight = FontWeights.Bold;
+            r.Foreground = new SolidColorBrush(Colors.Aqua);
+            r.FontSize = 22;
+
+
+            p.Inlines.Add(r);
+            fdDisplay.Blocks.Add(p);
+            rtbDisplay.Document = fdDisplay;
+
+
+        }
+
+        private void btnDisplay_Click(object sender, RoutedEventArgs e)
+        {
+            //add to flow document
+            string subjectLine = txtSubjectLine.Text;
+            string bodyText = runBody.Text;
+
+            Color userColor = UsersColor();
+
+            BlogPost bp = new BlogPost(subjectLine, bodyText, userColor);  
+            blogPosts.Add(bp);
+            DisplayBlogPosts(); //Update my dislay of blogposts
+
+            //fdDisplay.Blocks.Add(blogPosts[0].BlogPostFormatted());
+            UpdateRichTextBox(blogPosts[blogPosts.Count - 1]);  
+
+        }
+
+        public void DisplayBlogPosts()
+        {
+            //clear
+            lbBlogPost.Items.Clear();
+
+            foreach (BlogPost item in blogPosts)
+            {
+                lbBlogPost.Items.Add(item);
+            }
+        }
+
+        private void lbBlogPost_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            int selected = lbBlogPost.SelectedIndex;
+            if (selected >= 0)
+            {
+
+                //MessageBox.Show(blogPosts[selected].ToString());
+                BlogPost bp = blogPosts[selected];
+                UpdateRichTextBox(bp);
+
+            }
+
+
+        }
+        public Color UsersColor()
+        {
+            //parse rgb 
+            byte r = byte.Parse(txtR.Text);
+            byte g = byte.Parse(txtG.Text);
+            byte b = byte.Parse(txtB.Text);
+
+            Color userColor = Color.FromRgb(r, g, b);
+
+            return userColor;   
+
+        }
+
+
+        public void UpdateRichTextBox(BlogPost post)
+        {
+            fdDisplay.Blocks.Clear();
+            fdDisplay.Blocks.Add(post.BlogPostFormatted());
+        }
+
+    }//class
+}//namespace
